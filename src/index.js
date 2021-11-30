7b128c5d15bf1caa50a8ba41fbc9729d292c432b
dev-lester <lesters821@gmail.com>
2021-12-01 00:09:40 +0800

    Components and Props

--- src/index.js
+++ src/index.js
@@ -3,23 +3,45 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 
-function formatName(user) {
-  return user.firstName + ' ' + user.lastName;
+function Avatar(props) {
+  return (
+    <img className="Avatar" 
+      src={props.user.avatarUrl}
+      alt={props.user.name}
+    />
+  )
 }
 
-const user = {
-  firstName: 'Lester',
-  lastName: 'Sales'
-};
+function UserInfo(props) {
+  return (
+    <div className="UserInfo">
+      <Avatar user={props.name} />
+      <div className="UserInfo-name">
+        {props.user.name}
+      </div>
+    </div>
+  )
+}
 
-const element = (
-  <h1>
-    Hello, {formatName(user)}!
-  </h1>
-)
+function Comment(props) {
+  return (
+    <div className="Comment">
+      <UserInfo user={props.author} />
+      <div className="Comment-text">
+        {props.text}
+      </div>
+      <div className="Comment-date">
+        {formatDate(props.date)}
+      </div>
+    </div>
+  )
+}
 
 ReactDOM.render(
-  element,
+  Comment,
   document.getElementById('root')
 );
+
+
+
 reportWebVitals();
