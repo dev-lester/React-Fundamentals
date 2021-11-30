e357fb7a45ecf9f70c62ea976f253577e90f6220
dev-lester <lesters821@gmail.com>
2021-11-30 22:28:13 +0800

    Introducing JSX Rendering Elements

--- src/index.js
+++ src/index.js
@@ -3,36 +3,23 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 
-// function formatName(user) {
-//   return user.firstName + ' ' + user.lastName;
-// }
-
-// const user = {
-//   firstName: 'Lester',
-//   lastName: 'Sales'
-// };
-
-// const element = (
-//   <h1>
-//     Hello, {formatName(user)}!
-//   </h1>
-// )
-
-// ReactDOM.render(
-//   element,
-//   document.getElementById('root')
-// );
-
-function tick() {
-  const element = (
-    <div>
-      <h1>Hello, World!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
-    </div>
-  );
-  ReactDOM.render(element, document.getElementById('root'));
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
 }
 
-setInterval(tick, 1000);
+const user = {
+  firstName: 'Lester',
+  lastName: 'Sales'
+};
 
+const element = (
+  <h1>
+    Hello, {formatName(user)}!
+  </h1>
+)
+
+ReactDOM.render(
+  element,
+  document.getElementById('root')
+);
 reportWebVitals();
